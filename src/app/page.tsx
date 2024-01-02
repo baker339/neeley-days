d6829b9e45b43cee482a7c3c3b7c364ab822a3c4
qzbsct <Aidan.Baker@ally.com>
2024-01-02 10:41:24 -0500

    updated for Neeley stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,32 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+	var date1 = new Date();
+	var date2 = new Date("1/2/2024");
+	var diff = Math.abs(date1.getTime() - date2.getTime());
+	var diffDays = Math.ceil(diff / (1000 * 3600 * 24));
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+	return (
+		<main
+			style={{
+				display: "flex",
+				flexDirection: "column",
+				// justifyContent: "center",
+				// alignItems: "center",
+				marginTop: "5vh",
+			}}
+		>
+			<div style={{ fontSize: "15rem" }}>Days Without</div>
+			<div
+				style={{
+					fontSize: "15rem",
+					justifyContent: "space-between",
+					display: "grid",
+					gridTemplateColumns: "auto auto",
+					marginRight: "11vw",
+				}}
+			>
+				<div>Neeley</div>
+				<div>{diffDays}</div>
+			</div>
+		</main>
+	);
 }
