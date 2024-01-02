a39069045316921ebffcfd96c1e0bf6f945d65c1
qzbsct <Aidan.Baker@ally.com>
2024-01-02 16:53:08 -0500

    updated styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,16 @@ export default function Home() {
 	var diff = Math.abs(date1.getTime() - date2.getTime());
 	var diffDays = Math.ceil(diff / (1000 * 3600 * 24));
 
+	const images = [];
+	for (let i = 0; i < diffDays; i++) {
+		images.push(<img src="https://ca.slack-edge.com/EC088DZV2-WKT1YGYGJ-99e6d7e26867-512" style={{ width: "5rem" }} />);
+	}
+
 	return (
 		<main
 			style={{
 				display: "flex",
 				flexDirection: "column",
-				// justifyContent: "center",
-				// alignItems: "center",
 				marginTop: "5vh",
 			}}
 		>
@@ -27,6 +30,7 @@ export default function Home() {
 				<div>Neeley</div>
 				<div>{diffDays}</div>
 			</div>
+			<div style={{ maxWidth: "90vw" }}>{images}</div>
 		</main>
 	);
 }
