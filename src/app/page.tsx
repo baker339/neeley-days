68c48e632380706db70f936574fd1a61de510007
qzbsct <Aidan.Baker@ally.com>
2024-01-03 08:52:30 -0500

    updated to use state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,29 @@
-export default function Home() {
-	var date1 = new Date();
-	var date2 = new Date("1/2/2024");
-	var diff = Math.abs(date1.getTime() - date2.getTime());
-	var diffDays = Math.ceil(diff / (1000 * 3600 * 24));
+import { useState, useEffect } from "react";
 
-	const images = [];
-	for (let i = 0; i < diffDays; i++) {
-		images.push(<img src="https://ca.slack-edge.com/EC088DZV2-WKT1YGYGJ-99e6d7e26867-512" style={{ width: "5rem" }} />);
-	}
+export default function Home() {
+	const [diffDays, setDiffDays] = useState<number>(0);
+	const [images, setImages] = useState<React.JSX.Element[]>([]);
+
+	// const images = [];
+	// for (let i = 0; i < diffDays; i++) {
+	// 	images.push(<img src="https://ca.slack-edge.com/EC088DZV2-WKT1YGYGJ-99e6d7e26867-512" style={{ width: "5rem" }} />);
+	// }
+
+	useEffect(() => {
+		var date1 = new Date();
+		var date2 = new Date("1/2/2024");
+		var diff = Math.abs(date1.getTime() - date2.getTime());
+		var diffdaysinterim = Math.ceil(diff / (1000 * 3600 * 24));
+		setDiffDays(diffdaysinterim);
+	}, []);
+
+	useEffect(() => {
+		let ims = [];
+		for (let i = 0; i < diffDays; i++) {
+			ims.push(<img src="https://ca.slack-edge.com/EC088DZV2-WKT1YGYGJ-99e6d7e26867-512" style={{ width: "5rem" }} />);
+		}
+		setImages(ims);
+	}, [diffDays]);
 
 	return (
 		<main
